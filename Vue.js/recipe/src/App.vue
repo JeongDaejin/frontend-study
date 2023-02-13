31bad338f86dcafcf23e3ea768cf564a8b6571d1
JeongDaejin <123525505+JeongDaejin@users.noreply.github.com>
2023-02-13 17:56:40 +0900

    2월 13일 팀프로젝트 프론트

---
+++ Vue.js/recipe/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Header/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/My_Header.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ Vue.js/recipe/src/components/My_Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-app-bar absolute app light style="background-color: white;">
+       <dvi>
+        <v-img  src="../assets/logo.png" style="width: 40px;"></v-img>
+       </dvi>
+        <div style="margin: auto;">
+            <v-img style="width: 40px;"  src="../assets/logo.png" ></v-img>
+            <s_search>
+            <fieldset>
+            <input type="text"  value="" placeholder="찾을 내용을 입력하세요." >
+            <button type="button">검색</button>
+            </fieldset>
+            </s_search>
+        </div>
+    </v-app-bar>
+        <v-app-bar absolute app light style="background-color: white;">
+        <v-toolbar-title class="pl-12 ml-12" style="cursor: pointer;">
+        </v-toolbar-title>
+        <v-toolbar-items>
+            <v-btn>sadsadsadsadsa</v-btn>
+        </v-toolbar-items>
+        <v-toolbar-items>
+            <v-btn>종류</v-btn>
+            <v-btn>오메추</v-btn>
+            <v-btn>오메픽</v-btn>
+            <v-btn>랭킹</v-btn>
+        </v-toolbar-items>
+    </v-app-bar>
+</template>
+
+<script>
+export default{};
+</script>
+
+<style scope>
+div{
+    position: sticky;
+
+    border-bottom: 5px solid #ebebeb;
+}
+input{
+    width: 200px;
+    
+}
+</style>
